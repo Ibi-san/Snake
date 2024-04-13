858bce2f14f98cc3ef5daffbd1dfe565636e621c
Ibi-san <ibragim_nasirov@mail.ru>
2024-04-13 13:11:51 +0300

    Snake 1.3

--- Client/Snake/Assets/Scripts/Snake.cs
+++ Client/Snake/Assets/Scripts/Snake.cs
@@ -2,11 +2,25 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
+    [SerializeField] private Tail _tailPrefab;
     [SerializeField] private Transform _head;
-    [SerializeField] private float _spped = 2f;
+    [SerializeField] private float _speed = 2f;
     [SerializeField] private float _rotateSpped = 90f;
     private Vector3 _targetDirection = Vector3.zero;
+    private Tail _tail;
+    
+    public void Init(int detailCount)
+    {
+        _tail = Instantiate(_tailPrefab, transform.position, Quaternion.identity);
+        _tail.Init(_head, _speed, detailCount);
+    }
 
+    public void Destroy()
+    {
+        _tail.Destroy();
+        Destroy(gameObject);
+    }
+    
     private void Update()
     {
         Rotate();
@@ -19,7 +33,7 @@ public class Snake : MonoBehaviour
         _head.rotation = Quaternion.RotateTowards(_head.rotation, targetRotation, Time.deltaTime * _rotateSpped);
     }
 
-    private void Move() => transform.position += _head.forward * (Time.deltaTime * _spped);
+    private void Move() => transform.position += _head.forward * (Time.deltaTime * _speed);
 
     public void LookAt(Vector3 cursorPosition) => _targetDirection = cursorPosition - _head.position;
 }
--- Client/Snake/Assets/Scripts/Tail.cs
+++ Client/Snake/Assets/Scripts/Tail.cs
@@ -3,20 +3,63 @@ using UnityEngine;
 
 public class Tail : MonoBehaviour
 {
-    [SerializeField] private Transform _head;
-    [SerializeField] private List<Transform> _details;
+    [SerializeField] private Transform _detailPrefab;
     [SerializeField] private float _detailDistance = 1f;
-    [SerializeField] private float _snakeSpeed = 2f;
+    private Transform _head;
+    private float _snakeSpeed;
+    private List<Transform> _details = new();
     private List<Vector3> _positionHistory = new();
 
-    private void Awake()
+    public void Init(Transform head, float speed, int detailCount)
     {
+        _head = head;
+        _snakeSpeed = speed;
+        
+        _details.Add(transform);
         _positionHistory.Add(_head.position);
+        _positionHistory.Add(transform.position);
+        
+        SetDetailCount(detailCount);
+    }
 
-        foreach (var detail in _details)
+    private void SetDetailCount(int detailCount)
+    {
+        if (detailCount == _details.Count + 1) return;
+
+        int diff = (_details.Count - 1) - detailCount;
+
+        if (diff < 1)
         {
-            _positionHistory.Add(detail.position);
+            for (int i = 0; i < -diff; i++) 
+                AddDetail();
         }
+        else
+        {
+            for (int i = 0; i < diff; i++) 
+                RemoveDetail();
+        }
+    }
+
+    private void AddDetail()
+    {
+        Vector3 position = _details[_details.Count - 1].position;
+        Transform detail = Instantiate(_detailPrefab, position, Quaternion.identity);
+        _details.Insert(0, detail);
+        _positionHistory.Add(position);
+    }
+
+    private void RemoveDetail()
+    {
+        if (_details.Count <= 1)
+        {
+            Debug.LogError("Trying delete non-existing detail");
+            return;
+        }
+
+        Transform detail = _details[0];
+        _details.Remove(detail);
+        Destroy(detail.gameObject);
+        _positionHistory.RemoveAt(_positionHistory.Count - 1);
     }
 
     private void Update()
@@ -34,11 +77,14 @@ public class Tail : MonoBehaviour
         }
 
         for (int i = 0; i < _details.Count; i++)
-        {
             _details[i].position = Vector3.Lerp(_positionHistory[i + 1], _positionHistory[i], distance / _detailDistance);
+    }
 
-            Vector3 direction = (_positionHistory[i] - _positionHistory[i + 1]).normalized;
-            _details[i].position += direction * (Time.deltaTime * _snakeSpeed);
+    public void Destroy()
+    {
+        for (int i = 0; i < _details.Count; i++)
+        {
+            Destroy(_details[i].gameObject);
         }
     }
 }
