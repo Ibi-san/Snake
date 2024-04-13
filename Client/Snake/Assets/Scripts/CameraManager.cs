48a7ea48492752f15a3cc4631d4d3dbd436d5a9f
Ibi-san <ibragim_nasirov@mail.ru>
2024-04-14 01:24:13 +0300

    Snake 1.4

---
+++ Client/Snake/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Transform _camera;
+    private void Start()
+    {
+        _camera = Camera.main.transform;
+        _camera.parent = transform;
+        _camera.localPosition = Vector3.zero;
+    }
+
+    private void OnDestroy()
+    {
+        if (Camera.main == null) return; 
+        _camera.parent = null;
+    }
+}
